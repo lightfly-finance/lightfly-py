ffd3c5241334f60c8fa2079a9f9182d33691b5a0
twn39 <twn39@163.com>
2019-05-29 08:47:57 +0800

    update

--- tests/stock_test.py
+++ tests/stock_test.py
@@ -15,3 +15,22 @@ def test_hs300():
     data = stock.hs300()
     assert DataFrame == type(data)
 
+
+def test_daily_history():
+    data = stock.daily_history('sz002142', '2019-05-01', '2019-06-01')
+    assert DataFrame == type(data)
+
+
+def test_stock_realtime():
+    data = stock.stock_realtime('sz002142')
+    assert DataFrame == type(data)
+
+
+def test_hgs_trade_realtime():
+    data = stock.hgs_trade_realtime()
+    assert DataFrame == type(data)
+
+
+def test_hgtong_top10():
+    data = stock.hgtong_top10()
+    assert DataFrame == type(data)
